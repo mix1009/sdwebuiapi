0ea427f1455c46504284e444e0bf2b484a937832
ChunKoo Park <mix1009@gmail.com>
2023-02-24 15:42:10 +0900

    controlnet fix

--- webuiapi/__init__.py
+++ webuiapi/__init__.py
@@ -1,6 +1,6 @@
 from .webuiapi import WebUIApi, WebUIApiResult, Upscaler, HiResUpscaler, b64_img, raw_b64_img, ModelKeywordResult, ModelKeywordInterface, InstructPix2PixInterface, ControlNetInterface
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 __all__ = [
     "__version__",
--- webuiapi/webuiapi.py
+++ webuiapi/webuiapi.py
@@ -52,7 +52,7 @@ def b64_img(image: Image):
     img_base64 = 'data:image/png;base64,' + str(base64.b64encode(buffered.getvalue()), 'utf-8')
     return img_base64
 
-def raw_b64_img(x):
+def raw_b64_img(image: Image):
     # XXX controlnet only accepts RAW base64 without headers
     buffered = io.BytesIO()
     image.save(buffered, format="PNG")
